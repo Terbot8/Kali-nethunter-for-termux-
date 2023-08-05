cd35b3388bb75aea589c4323ed34f3d400333f84
Terbot8 <140831813+Terbot8@users.noreply.github.com>
2023-08-05 21:59:56 +0600

    Create kali-arm64.sh

---
+++ kali-arm64.sh
@@ -0,0 +1,4 @@
+# This is a default distribution plug-in.
+# Do not modify this file as your changes will be overwritten on next update.
+# If you want customize installation, please make a copy.
+DISTRO_NAME="kali-arm64"
